79b9be58afe46272a11e2534d469c332eb4b96f8
Bethany Walker <reklaw.bethany@gmail.com>
2018-10-26 21:11:56 -0400

    Completed part 3

--- server/app.js
+++ server/app.js
@@ -3,8 +3,8 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-app.use(express.static(path.join(__dirname, '../')))
-// app.use(express.static(path.join(__dirname, '..', 'public')))
+// app.use(express.static(path.join(__dirname, '../')))
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.get('*', function(req, res, next) {
   res.sendFile(path.join(__dirname, '..', 'index.html'))
